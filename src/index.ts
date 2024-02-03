97dcc45d10cc2a5a6d655111f42e662414ed5584
ansh mourya <anshmourya657@gmail.com>
2024-02-03 16:30:35 +0530

    cors error

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import database from "@config/database.js";
 import error from "@middelware/error.js";
 import router from "@routes/index.js";
-import auth from "@middelware/auth.js";
+
 const app = express();
 const port = 3000;
+
+app.use(
+  cors({ credentials: true, origin: true, exposedHeaders: ["Set-Cookie"] })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
 app.use(error);
+app.set("trust proxy", 1);
+
 app.get("/", async (req, res) => {
   res.send("ok");
 });
+
 database()
   .then(() => {
     app.listen(port, () =>
